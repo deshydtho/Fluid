c754f724a78db5489fa80248c5d832122183e019
Sutton <mysutton@micds.org>
2019-05-14 19:09:12 -0500

    uncommmented

--- createacchandler.php
+++ createacchandler.php
@@ -10,28 +10,28 @@ $fluidlastname=$_POST['lastname'];
 $fluidemail=$_POST['email'];
 $fluidphone=$_POST['phonenum'];
 
-// $sql="INSERT INTO fluid.user
-// (userid,
-// password,
-// dob,
-// firstname,
-// lastname,
-// email,
-// phonenum,
-// datejoin,
-// subend)
-// VALUES
-// ('$fluiduser',
-// '$fluidpass',
-// '$fluiddob',
-// '$fluidfirstname',
-// '$fluidlastname',
-// '$fluidemail',
-// '$fluidphone',
-// curdate(),
-// curdate()+30);
-// ";
-// echo $sql;
-// $con->query($sql);
-// echo $con;
+$sql="INSERT INTO fluid.user
+(userid,
+password,
+dob,
+firstname,
+lastname,
+email,
+phonenum,
+datejoin,
+subend)
+VALUES
+('$fluiduser',
+'$fluidpass',
+'$fluiddob',
+'$fluidfirstname',
+'$fluidlastname',
+'$fluidemail',
+'$fluidphone',
+curdate(),
+curdate()+30);
+";
+echo $sql;
+$con->query($sql);
+echo $con;
 ?>
